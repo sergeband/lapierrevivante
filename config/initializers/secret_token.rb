b955dc772ced30aa2d8e522226d086d01c44171e
sergeband <andaomejob@gmail.com>
2013-01-19 02:29:24 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lapierrevivante::Application.config.secret_token = 'cb8d5bef439ac35bc3f2d1e8b4604c97ee747ebe1d837a0ad32034dc87ffe93e43d199c6842e6d17ca8e6410e72afd46ed4bf71f74c6b2191d1317bc78289fca'
